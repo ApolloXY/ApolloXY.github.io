5bce18a03430913d98f235f1b07644bdd7f2464a
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2020-10-01 21:27:49 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -50,7 +50,7 @@ document.getElementById("btnLogin").onclick = function(){
      if (user != null) {
        //location.replace("https://apolloxy.github.io/home/");  
           get();
-      location.assign("https://apolloxy.github.io/home/index.html" + "?username=" + Username);
+      location.assign("https://apolloxy.github.io/home/" + "?username=" + Username);
       alert("Signed In " + em);
       
      }
