cb3144a6e83fb7ba772e01b98f21e19240aa2a7f
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2020-10-03 20:16:10 +0530

    Create loader.js

---
+++ home/loader.js
@@ -0,0 +1,8 @@
+function Loader() {
+  var yVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
