963371eff0b166ca06a37571ac379a7559a0670f
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2020-10-04 19:56:43 +0530

    Update Script.js

--- home/Script.js
+++ home/Script.js
@@ -4,6 +4,6 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  var scrolled = ((winScroll * 1.1) / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
