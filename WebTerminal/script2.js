dc499ff989296dfb2b8d1980818808f19ed66bed
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2021-01-19 21:34:02 +0530

    Update script2.js

--- WebTerminal/script2.js
+++ WebTerminal/script2.js
@@ -1,17 +1,17 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    document.getElementById("nav-log").style.padding = "30px";
-    document.getElementById("nav-log").style.fontSize = "30px";
+    document.getElementById("nav-log").style.padding = "40px";
+    document.getElementById("nav-log").style.fontSize = "50px";
 
-    document.getElementById("user-nav").style.fontSize = "30px";
+    document.getElementById("user-nav").style.fontSize = "50px";
 
     document.getElementById("terminalReslutsCont").style.position = "absolute";
     document.getElementById("terminalReslutsCont").style.top = "150px";
     document.getElementById("terminalReslutsCont").style.bottom = "100px";
-    document.getElementById("terminalReslutsCont").style.fontSize = "20px";
+    document.getElementById("terminalReslutsCont").style.fontSize = "50px";
 
-    document.getElementById("terminalTextInput").style.height = "50px";
+    document.getElementById("terminalTextInput").style.height = "100px";
     document.getElementById("terminalTextInput").style.padding = "30px";
-    document.getElementById("terminalTextInput").style.fontSize = "20px";
+    document.getElementById("terminalTextInput").style.fontSize = "50px";
 
 
   }else{
