7a1d38f3efc7cf3abf45f4e421a4224c942a08ef
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2021-01-19 22:26:31 +0530

    Update script2.js

--- WebTerminal/script2.js
+++ WebTerminal/script2.js
@@ -1,14 +1,19 @@
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     document.getElementById("nav-log").style.padding = "40px";
     document.getElementById("nav-log").style.fontSize = "50px";
+    document.getElementById("abc").style.fontSize = "50px";
 
-    document.getElementById("user-nav").style.fontSize = "70px";
+    document.getElementById("user-nav").style.fontSize = "60px";
+
     document.getElementById("nav-dec").style.backgroundImage = "repeating-linear-gradient(to right, transparent 0px, transparent 30px, 0px, #5bd1ff 60px)";
 
     document.getElementById("terminalReslutsCont").style.position = "absolute";
     document.getElementById("terminalReslutsCont").style.top = "150px";
     document.getElementById("terminalReslutsCont").style.bottom = "100px";
     document.getElementById("terminalReslutsCont").style.fontSize = "50px";
+
+    document.getElementsByClassName("userEnteredText").style.fontSize = "23px";
 
     document.getElementById("terminalTextInput").style.height = "100px";
     document.getElementById("terminalTextInput").style.padding = "30px";
@@ -18,6 +23,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   }else{
     document.getElementById("nav-log").style.padding = "10px";
     document.getElementById("nav-log").style.fontSize = "20px";
+    document.getElementById("abc").style.fontSize = "20px";
 
     document.getElementById("user-nav").style.fontSize = "20px";
     document.getElementById("nav-dec").style.backgroundImage = "repeating-linear-gradient(to right, transparent 0px, transparent 5px, 0px, #5bd1ff 10px)";
@@ -27,7 +33,11 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     document.getElementById("terminalReslutsCont").style.bottom = "60px";
     document.getElementById("terminalReslutsCont").style.fontSize = "14px";
 
+    document.getElementsByClassName("userEnteredText").style.fontSize = "14px";
+
     document.getElementById("terminalTextInput").style.height = "auto";
     document.getElementById("terminalTextInput").style.padding = "18px";
     document.getElementById("terminalTextInput").style.fontSize = "14px";
   }
+
+  
