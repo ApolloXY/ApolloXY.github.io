3d8e601857657dbef5f8090e0458c66ed875054d
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2021-01-20 08:46:00 +0530

    Update script2.js

--- WebTerminal/script2.js
+++ WebTerminal/script2.js
@@ -1,5 +1,7 @@
 function check_device(){
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
         location.replace("/WebTerminal/m");
+    }else{
+     location.replace("/WebTerminal/d");
     }
 }
