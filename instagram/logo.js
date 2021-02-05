2dd88f98821cd1524834732bb97d28120da0ac48
Atharva kolekar <36796602+ApolloXY@users.noreply.github.com>
2021-02-06 00:54:07 +0530

    Add files via upload

---
+++ instagram/logo.js
@@ -0,0 +1,15 @@
+document.getElementById("main").style.display = "block";
+  
+setTimeout(xy, 900);
+setTimeout(but, 1300);
+
+  function xy() {
+  document.getElementById("sub").style.display = "block";
+  document.getElementById("sub2").style.display = "block";
+ 
+
+  }
+
+  function but(){
+    document.getElementById("con3-button").style.display = "block";
+  }
